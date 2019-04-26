ea85183f343e7df87dbf52932ae6cf5d0c1cfb42
Caleb Greer <calebgreer18@gmail.com>
2019-04-26 16:04:27 -0400

    TankMovementComponent added

--- BattleTank/Source/BattleTank/Public/TankMovementComponent.h
+++ BattleTank/Source/BattleTank/Public/TankMovementComponent.h
@@ -7,11 +7,15 @@
 #include "TankMovementComponent.generated.h"
 
 /**
- * 
+ *
  */
-UCLASS()
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
 {
 	GENERATED_BODY()
-	
+
+public:
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void IntendMoveForward(float Throw);
+
 };
